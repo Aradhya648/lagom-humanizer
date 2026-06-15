73cac12ca2b2acf262495626323e2c7453c66e5c
Aradhya Mishra <aradhyamishra@Aradhyas-MacBook-Air.local>
2026-06-15 19:58:45 +0530

    fix: remove broken StatsPanel import, revert react version to clean state

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { StatsPanel } from "@/components/StatsPanel"
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Lagom Humanizer</h1>
-      <StatsPanel showDetailed={true} refreshInterval={30} />
     </main>
   )
 }
